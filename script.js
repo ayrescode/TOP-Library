681bf8eff47a8501fa81e046fb18613a327151fd
ayres <ayresmeirelles2@gmail.com>
2025-02-07 10:21:38 -0300

    refactoring of display function and creation of delete button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const library = document.querySelector('.library');
 const send = document.querySelector('.btn');
 
 // the constructor...
-function Book(name, author, lenght, read) {
+function book(name, author, lenght, read) {
   this.name = name;
   this.author = author;
   this.lenght = lenght;
@@ -12,30 +12,26 @@ function Book(name, author, lenght, read) {
 
 // take params, create a book then store it in the array
 function addBookToLibrary(name, author, lenght, read) {
-  let newBook = new Book(name, author, lenght, read);
+  let newBook = new book(name, author, lenght, read);
   return myLibrary.push(newBook);
 }
 
-// Displays books on the screen
-function bookshelf(array) {
-  const div = document.createElement('div');
-  const book = document.createElement('h1');
-  const author = document.createElement('p');
-  const pages = document.createElement('p');
-  const read = document.createElement('p');
-  book.textContent = `Book: ${array.name}`;
-  author.textContent = `By the author: ${array.author}`;
-  pages.textContent = `With ${array.lenght} pages`;
-  if (array.read === 'true') {
-    read.textContent = 'And i have read it!';
-  } else {
-    read.textContent = 'And i have NOT read it!';
+function displayBooks() {
+  library.innerHTML = '';
+  for (const book of myLibrary) {
+    const card = document.createElement('div');
+    card.setAttribute('data-index', myLibrary.indexOf(book));
+    card.innerHTML = `
+          <h1>${book.name}</h1>
+          <p>Author: ${book.author}</p>
+          <p>Pages: ${book.pages}</p>
+          <button class="${book.read}">  ${
+      book.read == 'true' ? 'Read' : 'Not Read'
+    }     </button>
+          <button class="delete">Delete</button>
+      `;
+    library.appendChild(card);
   }
-  library.appendChild(div);
-  div.appendChild(book);
-  div.appendChild(author);
-  div.appendChild(pages);
-  div.appendChild(read);
 }
 
 // button selector and event listener
@@ -52,11 +48,30 @@ function handleClick() {
     `${pages.value}`,
     `${select.value}`
   );
+  displayBooks();
   name.value = '';
   author.value = '';
   pages.value = '';
   select.value = 'true';
-
-  let lastBook = myLibrary[myLibrary.length - 1];
-  bookshelf(lastBook);
 }
+
+// delete button event
+library.addEventListener('click', (event) => {
+  if (event.target.classList.contains('delete')) {
+    const index = event.target.parentElement.getAttribute('data-index');
+    deleteBook(index);
+  }
+});
+// function delete
+function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
+// manual books created
+const book1 = new book('The Great Gatsby', 'F. Scott Fitzgerald', 180, false);
+const book2 = new book('To Kill a Mockingbird', 'Harper Lee', 281, false);
+const book3 = new book('No Longer Human', 'Osamu Dazai', 304, true);
+const book4 = new book('Flowers for Algernon', 'Daniel Keyes', 272, true);
+const book5 = new book('The Shining', 'Stephen King', 447, false);
+myLibrary.push(book1, book2, book3, book4, book5);
+displayBooks();
